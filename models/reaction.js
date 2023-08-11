d8e6f2453fad8bda297c4bcca3a120f6f14bfd10
Ren-Kun235 <changpobzeb651@gmail.com>
2023-08-11 18:29:13 -0500

    finished reaction schema

--- models/reaction.js
+++ models/reaction.js
@@ -3,23 +3,23 @@ const { Schema, model } = require('mongoose');
 const reactionSchema = new Schema(
     {
         reactionId: {
-            value: something,
-            default: new ObjectId,
-        },
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+          },
         reactionBody: {
             type: String,
             required: true,
-            max_length: 280,
+            maxlength: 280,
         },
         username: {
             type: String,
             required: true,
         },
         createdAt: {
-            date: new Date,
-            default: Date.now(),
-        }
-    }
+            type: Date,
+            default: Date.now,
+        },
+    },
 );
 
 module.exports = Reactions;
